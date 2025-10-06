b3d13430625b1fe7b941b2b9d989eaad13745d32
CASILYN S <casilynzirui@gmail.com>
2025-10-06 10:42:59 +0800

    01

--- 01/exercise2.py
+++ 01/exercise2.py
@@ -1,5 +1,12 @@
-input("What is your name?")
-print("Hello " + input("What is your name?"))
-
+# Declaring variables and input
 name = input("What is your name?")
 print(name)
+print(len(name))
+print("Hello " + name)
+
+print("Hello " + input("What is your name?"))
+
+# print(len(input("What is your username?")))
+username = input("What is your username?")
+length = len(username)
+print(length)
---
+++ 01/project1.py
@@ -0,0 +1,8 @@
+#Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in?\n")
+print(city)
+pet = input("What is your pet name?\n")
+print(pet)
+print("Your band name could be " + city + " " + pet)
